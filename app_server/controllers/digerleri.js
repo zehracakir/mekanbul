e8ecc19dc82492ad74be289510f4927c6c0d9252
zehracakir <cakirzehra312@gmail.com>
2022-10-14 14:24:10 +0300

    Main | Second Commit

---
+++ app_server/controllers/digerleri.js
@@ -0,0 +1,7 @@
+const hakkinda=function(req,res,next){
+    res.render('hakkinda',{title: 'Hakkında Sayfası'});
+};
+
+module.exports={
+    hakkinda
+}
---
+++ app_server/controllers/mekanlar.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+const anaSayfa=function(req,res,next){
+    res.render('anaSayfa',{title: 'Anasayfa'});
+};
+
+const mekanBilgisi=function(req,res,next){
+    res.render('mekanbilgisi',{title: 'Mekan Bilgisi'});
+};
+
+const yorumEkle=function(req,res,next){
+    res.render('yorumekle',{title: 'Yorum Ekle'});
+};
+
+module.exports={
+    anaSayfa,
+    mekanBilgisi,
+    yorumEkle
+}
