2d470f656722867a24ba21dfd56d004e02ad0efd
zehracakir <cakirzehra312@gmail.com>
2022-11-10 11:03:12 +0300

    Odev4 | First Commit

---
+++ app_server/models/db.js
@@ -0,0 +1,27 @@
+var mongoose=require("mongoose");
+require("./mekansema")
+var dbURI="mongodb://localhost/mekanbul";
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback(); b
+    });
+}
+process.on("SIGINT",function(){
+    kapat("Uygulama kapatıldı!",function(){
+        process.exit(0);
+    })
+})
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+" adresindeki veritabanına bağlandı");
+}
+);
+mongoose.connection.on("disconnected",function(){
+    console.log(dbURI+" adresindeki veritabanı bağlantısı koptu");
+}
+);
+mongoose.connection.on("error",function(){
+    console.log("Bağlantı hatası");
+}
+);
